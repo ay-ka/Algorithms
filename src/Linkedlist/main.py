e9360b4f4086cd231943651b9c3f2f56563b0299
ay-ka <aydinkazempour7@gmail.com>
2022-07-01 21:43:50 +0430

    push codes from local directory to github

---
+++ src/Linkedlist/main.py
@@ -0,0 +1,231 @@
+from ast import operator
+from collections import OrderedDict
+import numpy as np
+import math
+import pdb
+from abc import ABC, abstractmethod
+from collections import Counter
+import argparse
+import copy
+
+class linkedList():
+    
+    def __init__(self, value):
+        
+        self.value = value
+        
+        self.next = None
+        
+        self.head_key = None
+        
+
+
+
+class Operation:
+    
+    def __init__(self):
+        
+        self.linked_list = {}
+    
+    def GetData(self, path):
+            
+        data = OrderedDict()
+        
+        with open(path, "r") as f:
+            
+            line = f.readline()
+            
+            line = line.strip()
+            
+            key_value = line.split()
+            
+            while line != "":
+            
+                data[key_value[0]] = key_value[1]
+                
+                line = f.readline()
+                
+                line = line.strip()
+                
+                key_value = line.split()
+                
+        return data 
+    
+    def create_list(self, data):
+        
+        keys = list(data.keys())
+        
+        self.head = linkedList(data.pop(keys[0]))
+        
+        self.head.head_key = keys[0]
+        
+        self.linked_list = {data_key : linkedList(data_value) for data_key, data_value in data.items()}
+        
+        keys = list(self.linked_list.keys())
+        
+        self.head.next = self.linked_list[keys[0]]
+        
+        #########
+        
+        for idx, key in enumerate(keys):
+            
+            try:
+            
+                self.linked_list[keys[idx]].next = self.linked_list[keys[idx + 1]]
+                
+            except : pass
+    
+    def insert(self, head, LinkedList, index, value):
+        
+        current_node = head
+        
+        for iteration in range(index - 2):
+            
+            current_node = current_node.next
+            
+        next_ = current_node.next
+        
+        new_node = LinkedList(value)
+            
+        current_node.next = new_node
+        
+        new_node.next = next_
+            
+    def delete(self, head, index):
+        
+        current_node = head
+        
+        for iteration in range(index - 2):
+            
+            current_node = current_node.next
+            
+        current_node.next = current_node.next.next
+    
+    def reverse(self, head):
+        
+        prev = None
+        
+        keys = list(self.linked_list.keys())
+        
+        current = head
+        
+        while current != None:
+            
+            next_ = current.next
+            
+            current.next = prev
+            
+            prev = current
+            
+            current = next_
+            
+        head_candidate = self.linked_list.pop(list(self.linked_list.keys())[-1])
+        
+        self.linked_list[self.head.head_key] = copy.deepcopy(self.head)
+        
+        head_candidate.head_key = keys[-1]
+        
+        self.head = head_candidate
+    
+    def findValue(self, head, index):
+        
+        current_node = head
+        
+        count = 0
+        
+        while head != None:
+            
+            if count == index - 1:
+                
+                return current_node.value
+            
+            current_node = current_node.next
+            
+            count += 1
+        
+        return "no value"
+    
+    def findIndex(self, head, value):
+        
+        current_node = head
+        
+        count = 0
+        
+        while current_node != None:
+            
+            if current_node.value == value:
+                
+                return count + 1
+            
+            count += 1
+            
+            current_node = current_node.next
+                
+        return "no value"
+
+
+
+if __name__ == "__main__":
+
+    # get parameters from terminal
+    
+    LinkedList_Parser = argparse.ArgumentParser()
+    
+    #linkedlist
+    
+    LinkedList_Parser.add_argument("--value", help='find index of value', type = str, default = None)
+    
+    LinkedList_Parser.add_argument("--index", help='find value by index', type = int, default = None)
+    
+    LinkedList_Parser.add_argument("--delete_val_index", help='delete value to this index', type = int, default = None)
+    
+    LinkedList_Parser.add_argument("--insert_index", help='insert value to this index', type = int, default = None)
+    
+    LinkedList_Parser.add_argument("--insert_value", help='insert this value to defined index by --insert_index', default = None, type = str)
+    
+    LinkedList_Parser.add_argument("--reverse", help='insert this value to defined index', action = "store_true")
+
+    LinkedList_Parser.add_argument("--data_path", default = "src/LinkedList/data.txt", help='path to data', type = str)
+    
+    # parse args
+    
+    args = LinkedList_Parser.parse_args()
+    
+    # run
+        
+    runner = Operation()
+    
+    data = runner.GetData(args.data_path)
+    
+    runner.create_list(data)
+    
+    if args.value != None:
+        
+        print(f" value {args.value} found in index {runner.findIndex(head = runner.head, value = args.value)} ")
+        
+    if args.index != None:
+        
+        print(f"value {runner.findValue(head = runner.head, index = args.index)} found in index {args.index} ")
+        
+    if args.delete_val_index != None:
+        
+        runner.delete(head = runner.head, index = args.delete_val_index)
+        
+        print("requested node deleted")
+        
+    if args.reverse:
+        
+        runner.reverse(head = runner.head)  
+        
+        print(f"list reversed and now head's key is : {runner.head.head_key} and head's value is : {runner.head.value}") 
+        
+    if args.insert_index and args.insert_value:
+    
+        runner.insert(head = runner.head, LinkedList = linkedList, index = args.insert_index, value = args.insert_value)  
+    
+        print(f" value {args.insert_value} inserted in index {args.insert_index}")
+
+        
+        
+
+
---
+++ src/UCS/main.py
@@ -0,0 +1,291 @@
+import argparse
+import numpy
+import math
+import pdb
+from abc import ABC, abstractmethod
+
+class Algo(ABC):
+    
+    def __init__(self):
+        
+        """
+            
+        base class to inherite
+
+        Args:
+
+            no argument
+
+        Returns:
+
+            no return --> just iniialize algorithm
+
+        """
+        
+        pass
+    
+    def getData(self, file_path):
+        
+        """
+            
+        get data to create graph (array representation of graph)
+
+        Args:
+
+            file_path : directory which file is stored (txt.file)
+
+        Returns:
+
+            data : data created from text file (array representation of graph)
+
+        """
+        
+        data = {}
+        
+        with open(file_path, "r") as f:
+            
+            line = f.readline()  
+            
+            line = line.strip()
+            
+            key_value_cost = line.split()
+            
+            while line != "":
+                
+                key, value, cost = key_value_cost[0], key_value_cost[1], key_value_cost[2]
+                
+                if data.get(key) is None and data.get(value) is None:
+
+                    data[key] = {value : int(cost)}
+                    
+                    data[value] = {key : int(cost)}
+
+                elif data.get(key) is None:
+                    
+                    data[key] = {value : int(cost)}
+                    
+                    data[value].update({key : int(cost)})
+                    
+                elif data.get(value) is None:
+                    
+                    data[value] = {key : int(cost)}
+                    
+                    data[key].update({value : int(cost)})
+                    
+                else:
+                    
+                    data[value].update({key : int(cost)})
+
+                    data[key].update({value : int(cost)})
+
+                line = f.readline()
+
+                line = line.strip()
+
+                key_value_cost = line.split()
+
+        return data
+    
+    @abstractmethod
+    def Search(self, data, start, goal):
+        
+        """
+            
+        method for main part of algorithm (Search)
+
+        Args:
+
+            data : array representation of graph 
+            
+            start : start node to begin (number in string format)
+            
+            goal : goal node to raech (number in string format)
+
+        Returns:
+
+            solutions : dictionarycontaining all the solutions available
+
+        """
+        
+        pass
+    
+    @abstractmethod
+    def checkVisited(self):
+        
+        """
+            
+        check node in adjacency nodes is visited before or not
+        
+        Args:
+
+            adjacency_nodes : all node in adjacency of target node
+            
+            visited : list of all visited node up to now
+        Returns:
+
+            list of all nodes in adjacency_node list which haven't visited yet
+
+        """
+        
+        pass
+    
+    
+class UCS(Algo):
+    
+    def __init__(self):
+        
+        """
+            
+        base class for running algorithm
+
+        Args:
+
+            no arument
+
+        Returns:
+
+            no return just initialize algorithm
+
+        """
+        
+        super(UCS, self).__init__()
+        
+        
+        
+    def Search(self, data, start, goal):
+        
+        """
+            
+        method for main part of algorithm (Search)
+
+        Args:
+
+            data : array representation of graph 
+            
+            start : start node to begin (number in string format)
+            
+            goal : goal node to raech (number in string format)
+
+        Returns:
+
+            solutions : dictionarycontaining all the solutions available
+
+        """
+        
+        visited, options, solutions = [], {}, {}
+        
+        current_node = start
+        
+        visited.append(start)
+        
+        adjacency_nodes = list(data.get(current_node).keys())
+        
+        valid_keys = self.checkVisited(adjacency_nodes, visited)
+        
+        options = {(current_node, key) : data.get(current_node)[key] for key in valid_keys}
+        
+        index = 1
+
+        while len(list(options.keys())):
+            
+            (cost, path) = self.pickNode(options)
+            
+            current_node = path[-1]
+            
+            visited.append(current_node)
+            
+            if current_node == goal:
+                
+                solutions["solution_" + str(index)] = {"path" : path, "cost" : cost}
+                
+                index += 1
+                
+                del options[path]
+                
+                continue
+            
+            if data.get(current_node) is None:
+                
+                del options[path]
+                
+            else:
+            
+                adjacency_nodes = list(data.get(current_node).keys())
+            
+                valid_keys = self.checkVisited(adjacency_nodes, visited)
+            
+                path_cost = options[path]
+            
+                del options[path]
+            
+                options.update({path + (key,) : path_cost + data.get(current_node)[key] for key in valid_keys})
+                
+        return solutions
+    
+    def checkVisited(self, adjacency_nodes, visited):
+        
+        """
+            
+        check node in adjacency nodes is visited before or not
+        
+        Args:
+
+            adjacency_nodes : all node in adjacency of target node
+            
+            visited : list of all visited node up to now
+        Returns:
+
+            list of all nodes in adjacency_node list which haven't visited yet
+
+        """
+        
+        return list(filter(lambda x : not(x in visited), adjacency_nodes))
+    
+    def pickNode(self, options):
+        
+        """
+            
+        select next node from path availbale with minimum cost
+
+        Args:
+
+            options : options currently available to choose one path from
+
+        Returns:
+
+            a tuple containing path with minimum cost available to choose and derive node from it alongside the cost of it
+
+        """
+      
+        selection_list = [(cost, path) for path, cost in options.items()]
+        
+        selection_list = sorted(selection_list)
+        
+        return selection_list[0]
+    
+    
+if __name__ == "__main__":
+    # get parameters from terminal
+    
+    UCS_Parser = argparse.ArgumentParser()
+    
+    #bfs
+    
+    UCS_Parser.add_argument("--start", default="1", help='start node', type = str)
+        
+    UCS_Parser.add_argument("--goal", default="2",  help='goal node', type = str)
+    
+    UCS_Parser.add_argument("--data_path", default="src/UCS/data.txt",  help='path to data', type = str)
+
+    
+    # parse args
+    
+    args = UCS_Parser.parse_args()
+    
+    # run
+        
+    runner = UCS()
+    
+    data = runner.getData(args.data_path)
+
+    print(runner.Search(data, start = args.start, goal = args.goal))
